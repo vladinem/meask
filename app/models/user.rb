261f1e402e29b27b1072c483d4c01f68a0e65ae5
Vladimir I. Nemytchenko <vnemytch.belgrad@gmail.com>
2022-11-17 17:36:05 +0000

    Change controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,41 @@ require 'openssl'
 
 class User < ActiveRecord::Base
 
-  has_many :questions
+  has_many :questions# Модель пользователя.
+  class User < ActiveRecord::Base
+    ITERATIONS = 20_000
+    DIGEST = OpenSSL::Digest::SHA256.new
+  
+    attr_accessor :password
+  
+    has_many :questions
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
-
-  attr_accessor :password
 
   # валидация будет проходить только при создании нового юзера
   validates :password, presence: true, on: :create
 
   # и поле подтверждения пароля
   validates_confirmation_of :password
+  
+  before_save :encrypt_password
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
 
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
 
    # Основной метод для аутентификации юзера (логина). Проверяет email и пароль,
   # если пользователь с такой комбинацией есть в базе, возвращает этого
@@ -42,5 +64,4 @@ class User < ActiveRecord::Base
     # Иначе, возвращаем nil
     nil
   end
-
 end
